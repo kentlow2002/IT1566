21d382566dcc846294a61c4387d0ed8aa16e65a6
kent low <kentlow2002@gmail.com>
2020-01-24 13:09:07 +0800

    premerge

--- Product.py
+++ Product.py
@@ -1,44 +1,71 @@
 import random as rand
 
 
-class Products:
-    def __init__(self,productId,productName,productStock,productPrice,productDesc):
-        self.__productId = 0 #differentiates products with same name
+class Product:
+    countId = 0
+
+    def __init__(self, productName, productCondition, productPrice, productQuantity, productDescription, productPicture):
+        Product.countId += 1
+        self.__productId = Product.countId  # differentiates products with same name
         self.__productName = productName
-        self.__productStock = productStock
+        self.__productCondition = productCondition
+        self.__productQuantity = productQuantity
         self.__productPrice = productPrice
-        self.__productDesc = productDesc
+        self.__productDescription = productDescription
+        self.__productPicture = productPicture
+
 
     def productBuy(self):
         return 0
 
     def productSell(self):
         return 0
-    def get_productID(self):
+
+    # accessor
+
+    def get_productId(self):
         return self.__productId
+
     def get_productName(self):
         return self.__productName
-    def set_productName(self, productName):
-        self.__productName = productName
-    def get_productStock(self):
-        return self.__productStock
-    def set_productStock(self, productStock):
-        self.__productName = productStock
+
+    def get_productCondition(self):
+        return self.__productCondition
+
+    def get_productQuantity(self):
+        return self.__productQuantity
+
     def get_productPrice(self):
         return self.__productPrice
+
+    def get_productDescription(self):
+        return self.__productDescription
+
+    def get_productPicture(self):
+        return self.__productPicture
+
+
+    # accessor
+
+    def set_productId(self, productId):
+        self.__productId = productId
+
+    def set_productName(self, productName):
+        self.__productName = productName
+
+    def set_productCondition(self, productCondtion):
+        self.__productCondition = productCondtion
+
+    def set_productQuantity(self, productQuantity):
+        self.__productQuantity = productQuantity
+
     def set_productPrice(self, productPrice):
         self.__productPrice = productPrice
-    def get_productDesc(self):
-        return self.__productDesc
-    def set_productDesc(self, productDesc):
-        self.__productDesc = productDesc
-class ProductList:
-    def __init__(self):
-        self.__list = []
 
-    def addProduct(self,productId):
-        #self.__list.append(product)
-        return 0
-    '''def rmProduct(self):
-            return 0
-            this function is to remove product. commented away for consideration'''
+    def set_productDesc(self, productDescription):
+        self.__productDescription = productDescription
+
+    def set_productPicture(self, productPicture):
+        self.__productPicture = productPicture
+
+
