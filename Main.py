1bc4be1829724ab8134a2534ef0cd90d7939518f
BurningAlpha <8577@hotmail.sg>
2020-02-09 23:44:09 +0800

    pushing

--- Main.py
+++ Main.py
@@ -896,6 +896,7 @@ def faq():
         return redirect(url_for("index"))
     return render_template('FAQs.html', faqList=faqList, usertype = current_user.getType(), UserID = current_user.getID())
 @app.route('/faqType') #R faq by user type
+@login_required
 def faqTypeS():
     if current_user.is_authenticated:
         db = shelve.open("faq.db", "c")
@@ -917,6 +918,7 @@ def faqTypeS():
     return render_template('FAQtype.html', faqList=faqList, usertype = current_user.getType(), UserID = current_user.getID())
 
 @app.route('/faq/ask', methods=['GET', 'POST'])
+@login_required
 def ask():
     if current_user.is_authenticated:
         createFaqForm = FaqForm(request.form)
